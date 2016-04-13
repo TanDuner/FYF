d1ce31e5b74768c37bff7d77157472a22a5403c9
徐书传 <xushuchuan@58.com>
2016-04-13 13:33:33 +0800

    分一分 第一次提交

---
+++ FYF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FYF
+//
+//  Created by 徐书传 on 16/4/13.
+//  Copyright © 2016年 Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FYF/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FYF
+//
+//  Created by 徐书传 on 16/4/13.
+//  Copyright © 2016年 Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
